1c0dc6c220d2c21b894290e6332ba779d64036b7
Dennis Kipping <dennis.kipping@student.hpi.uni-potsdam.de>
2017-11-23 17:29:59 +0100

    is luigi task now

--- src/file_lister.py
+++ src/file_lister.py
@@ -3,9 +3,13 @@
 
 import os
 import json
+import luigi
+
+SOURCE_DIR = "example-txts"
+TARGET_DIR = "example-txts"
 
 
-class FileLister():
+class FileLister(luigi.Task):
     """
     A class for parsing files to json dicts and dumping them to a list.
 
@@ -35,12 +39,20 @@ class FileLister():
         json_string = json.dumps(dict_string)
         return json_string
 
-    def dump_files(self, ending, source_dir, target_dir):
+    def list_files(self, ending, source_dir, target_dir):
         """Given ending and source dir, dump a list of all matching files in source dir as json objects."""
         found_files = self.find_files_in_dir(ending, source_dir)
 
-        with open(target_dir + 'example-txts/txtfiles.txt', 'w') as outfile:
+        with open(target_dir + '/txtfiles.txt', 'w') as outfile:
             for f in found_files:
                 with open(f) as infile:
                     json_string = self.parse_file_to_json(infile)
                     outfile.write(json_string + '\n')
+
+    def output(self):
+        """File the list of json objects in a txtfiles textfile."""
+        return luigi.LocalTarget(TARGET_DIR + '/txtfiles.txt')
+
+    def run(self):
+        """Run the listing in the Luigi Task."""
+        self.list_files('.txt', SOURCE_DIR, TARGET_DIR)
