4954f0590116b8fc67f5f88b9181bf1454ed27b6
Dennis Kipping <dennis.kipping@student.hpi.uni-potsdam.de>
2017-11-23 16:52:04 +0100

    added txt concat functionality, for now no luigi job

---
+++ src/concat-txt/concat.py
@@ -0,0 +1,46 @@
+"""This module can parse files to json dicts and dump a list of them to txt."""
+
+
+import os
+import json
+
+
+class FileLister():
+    """
+    A class for parsing files to json dicts and dumping them to a list.
+
+    Functionalities:
+    * read all files of given dir with given ending
+    * parse to json dicts
+    * dump to one list in file on given dir
+    """
+
+    def find_files_in_dir(self, ending, source_dir):
+        """Given ending and path to dir as a string, return list of filenames."""
+        directory = os.fsencode(source_dir)
+        files = []
+
+        for f in os.listdir(directory):
+            filename = os.fsdecode(f)
+            if filename.endswith(ending):
+                files.append(filename)
+
+        return files
+
+    def parse_file_to_json(self, file):
+        """Encode file as json string."""
+        dict_string = "{'raw':'"
+        dict_string += file.read()
+        dict_string += "'}"
+        json_string = json.dumps(dict_string)
+        return json_string
+
+    def dump_files(self, ending, source_dir, target_dir):
+        """Given ending and source dir, dump a list of all matching files in source dir as json objects."""
+        found_files = self.find_files_in_dir(ending, source_dir)
+
+        with open(target_dir + '/txtfiles.txt', 'w') as outfile:
+            for f in found_files:
+                with open(f) as infile:
+                    json_string = self.parse_file_to_json(infile)
+                    outfile.write(json_string + '\n')
