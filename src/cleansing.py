7dd9a6f5e48584a74118db1604c722f27d242683
Jonas Hering <jonas.hering@student.hpi.de>
2017-11-30 14:28:37 +0100

    Add data cleaner made with talon, ref meta#98

---
+++ src/cleansing.py
@@ -0,0 +1,51 @@
+"""This module is responsible for email cleansing."""
+
+import luigi
+from spark_pipeline import EmailBodyExtractor
+# import findspark
+# findspark.init()
+from pyspark import SparkContext
+from datetime import datetime
+import json
+import talon
+talon.init()
+from talon import quotations
+from talon.signature.bruteforce import extract_signature
+
+
+DATETIMESTAMP = datetime.now().strftime('%Y-%m-%d_%H:%M')
+
+
+class EmailCleaner(luigi.Task):
+    """This task uses Talon from Mailgun to clean emails."""
+
+    def requires(self):
+        """Dependency: FileLister like file with field: body."""
+        return EmailBodyExtractor()
+
+    def output(self):
+        """Write a HDFS target with timestamp."""
+        return luigi.contrib.hdfs.HdfsTarget('/pipeline/cleaner/' + DATETIMESTAMP + '_cleaner.txt')
+
+    def run(self):
+        """Run luigi task."""
+        sc = SparkContext()
+        data = open(self.input().path).read().splitlines()
+        myRdd = sc.parallelize(data)
+        results = myRdd.map(lambda x: self.clean_entry(x)).collect()
+        with self.output().open('w') as f:
+            for result in results:
+                f.write(result + '\n')
+        sc.stop()
+
+    def clean_entry(self, entry):
+        """Read body, and clean it of noise using Talon."""
+        dict = json.loads(entry)
+        body = dict['full_body'].replace('\\n', '\n')
+        reply = quotations.extract_from_plain(body)
+        text, signat = extract_signature(reply)
+        dict['reply'] = reply
+        dict['reply_text'] = text
+        # dict['signature'] = signat
+
+        return json.dumps(dict, ensure_ascii=False)
