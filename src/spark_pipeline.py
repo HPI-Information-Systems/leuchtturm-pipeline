f22e216d6715f20c1d79cede09ba84f8269cbef9
Jonas Hering <jonas.hering@student.hpi.de>
2017-11-30 22:02:33 +0100

    Use better var names, adapt docstrings to actual tasks

--- src/spark_pipeline.py
+++ src/spark_pipeline.py
@@ -1,4 +1,4 @@
-"""This module can parse files to json dicts and dump a list of them to txt."""
+"""This module processes emails."""
 
 
 import os
@@ -13,28 +13,29 @@ from datetime import datetime
 import emailbody.extractmailbody as body_extractor
 from langdetect import detect
 import email as email
-
 import talon
 talon.init()
 from talon import quotations
 from talon.signature.bruteforce import extract_signature
 
 
-
 DATETIMESTAMP = datetime.now().strftime('%Y-%m-%d_%H-%M')
 
 
 class FileLister(luigi.Task):
-    """
-    A class for parsing files to json dicts and dumping them to a list.
-
-    Functionalities:
-    * read all files of given dir with given ending
-    * parse to json dicts
-    * dump to one list in file on given dir
-    """
+    """A task for parsing files to json dicts and dumping them to a list."""
 
     source_dir = luigi.Parameter(default="./../tests/example-txts/")
+
+    def output(self):
+        """Write a HDFS target with timestamp."""
+        return luigi.contrib.hdfs.HdfsTarget('/pipeline/emails_concatenated/' +
+                                             DATETIMESTAMP +
+                                             '_emails_concatenated.txt')
+
+    def run(self):
+        """Run the listing."""
+        self.list_files('.txt')
 
     def find_files_in_dir(self, ending):
         """Given ending and path to dir as a string, return list of filenames."""
@@ -56,16 +57,6 @@ class FileLister(luigi.Task):
                                    ensure_ascii=False) +
                         '\n')
 
-    def output(self):
-        """File the list of json objects in a txtfiles textfile."""
-        return luigi.contrib.hdfs.HdfsTarget('/pipeline/emails_concatenated/' +
-                                             DATETIMESTAMP +
-                                             '_emails_concatenated.txt')
-
-    def run(self):
-        """Run the listing in the Luigi Task."""
-        self.list_files('.txt')
-
 
 class EnronFooterRemover(luigi.Task):
     """Given a filepath, this task removes the footer that was added after the initial export of the data set."""
@@ -80,7 +71,7 @@ class EnronFooterRemover(luigi.Task):
         return FileLister()
 
     def output(self):
-        """Override file at given path without footer."""
+        """Write a HDFS target with timestamp."""
         return luigi.contrib.hdfs.HdfsTarget('/pipeline/footer_removed/' +
                                              DATETIMESTAMP +
                                              '_footer_removed.txt')
@@ -89,7 +80,7 @@ class EnronFooterRemover(luigi.Task):
         """Execute footer removal."""
         sc = SparkContext()
         data = sc.textFile(self.input().path)
-        #result = data.map(lambda x: self.remove_footer(x)).saveAsTextFile(self.output().path)
+        # result = data.map(lambda x: self.remove_footer(x)).saveAsTextFile(self.output().path)
         results = data.map(lambda x: self.remove_footer(x)).collect()
         with self.output().open('w') as f:
             for result in results:
@@ -98,20 +89,20 @@ class EnronFooterRemover(luigi.Task):
 
     def remove_footer(self, input):
         """Replace footer with empty string."""
-        dict = json.loads(input)
-        dict['body'] = dict['full_body'].replace(self.footer, '')
-        return json.dumps(dict, ensure_ascii=False)
+        document = json.loads(input)
+        document['body'] = document['full_body'].replace(self.footer, '')
+        return json.dumps(document, ensure_ascii=False)
 
 
 class MetadataExtractor(luigi.Task):
     """This bad boy gets all them metadatas bout y'alls emails."""
 
     def requires(self):
-        """Raw email data."""
+        """Expect raw email data."""
         return EnronFooterRemover()
 
     def output(self):
-        """Add metadata to each mail."""
+        """Write a HDFS target with timestamp."""
         return luigi.contrib.hdfs.HdfsTarget('/pipeline/metadata_extracted/' +
                                              DATETIMESTAMP +
                                              '_metadata_extracted.txt')
@@ -128,23 +119,23 @@ class MetadataExtractor(luigi.Task):
 
     def extract_metadata(self, input):
         """Extract meta data of each email."""
-        dict = json.loads(input)
-        message = email.message_from_string(dict["full_body"])
+        document = json.loads(input)
+        message = email.message_from_string(document["full_body"])
         for metainfo in message.keys():
-            dict[metainfo] = message[metainfo]
+            document[metainfo] = message[metainfo]
 
-        return json.dumps(dict, ensure_ascii=False)
+        return json.dumps(document, ensure_ascii=False)
 
 
 class EmailBodyExtractor(luigi.Task):
     """This bad boy gets all them email booti bout y'alls emails."""
 
     def requires(self):
-        """Raw email data."""
+        """Expect raw email data."""
         return MetadataExtractor()
 
     def output(self):
-        """Add pure body to each mail."""
+        """Write a HDFS target with timestamp."""
         return luigi.contrib.hdfs.HdfsTarget('/pipeline/emailbody_extracted/' +
                                              DATETIMESTAMP +
                                              '_emailbody_extracted.txt')
@@ -161,8 +152,8 @@ class EmailBodyExtractor(luigi.Task):
 
     def get_body(self, input):
         """Extract and return the body of an email."""
-        dict = json.loads(input)
-        mail_text = dict['full_body']
+        document = json.loads(input)
+        mail_text = document['full_body']
         text_lines = mail_text.splitlines()
 
         func_b = body_extractor.enron_two_zone_line_b_func
@@ -178,16 +169,16 @@ class EmailBodyExtractor(luigi.Task):
             if int(head_body_indicator[i][1][0]) == int(1.0):
                 body_text += head_body_indicator[i][0]
 
-        dict['body'] = body_text
+        document['body'] = body_text
 
-        return json.dumps(dict, ensure_ascii=False)
+        return json.dumps(document, ensure_ascii=False)
 
 
 class EmailCleaner(luigi.Task):
     """This task uses Talon from Mailgun to clean emails."""
 
     def requires(self):
-        """Dependency: FileLister like file with field: body."""
+        """Require FileLister like file with field: body."""
         return EmailBodyExtractor()
 
     def output(self):
@@ -197,7 +188,7 @@ class EmailCleaner(luigi.Task):
                                              '_emails_cleaned.txt')
 
     def run(self):
-        """Run luigi task."""
+        """Run cleansing task."""
         sc = SparkContext()
         data = sc.textFile(self.input().path)
         results = data.map(lambda x: self.clean_entry(x)).collect()
@@ -206,38 +197,34 @@ class EmailCleaner(luigi.Task):
                 f.write(result + '\n')
         sc.stop()
 
-    def clean_entry(self, entry):
-        """Read body, and clean it of noise using Talon."""
-        dict = json.loads(entry)
-        body = dict['body'].replace('\\n', '\n')
+    def clean_entry(self, input):
+        """Clean one entry."""
+        document = json.loads(input)
+        body = document['body'].replace('\\n', '\n')
         reply = quotations.extract_from_plain(body)
         text, signat = extract_signature(reply)
-        dict['reply'] = reply
-        dict['reply_text'] = text
+        document['reply'] = reply
+        document['reply_text'] = text
         # dict['signature'] = signat
 
-        return json.dumps(dict, ensure_ascii=False)
+        return json.dumps(document, ensure_ascii=False)
 
 
 class LanguageDetector(luigi.Task):
-    """
-    Given a filepath, this task extracts the body of an e-mail and writes it to a file at the target path.
-
-    This job uses Quagga by Tim Repke.
-    """
+    """This task detects the language of a text using langdetect."""
 
     def requires(self):
         """Require e-mail text without headers and footer."""
         return EmailCleaner()
 
-    def detect_language(self, input):
-        """Add language to each entry."""
-        dict = json.loads(input)
-        dict['lang'] = detect(dict['full_body'])
-        return json.dumps(dict, ensure_ascii=False)
+    def output(self):
+        """Write a HDFS target with timestamp."""
+        return luigi.contrib.hdfs.HdfsTarget('/pipeline/language_detected/' +
+                                             DATETIMESTAMP +
+                                             '_language_detected.txt')
 
     def run(self):
-        """Replace footer with empty string."""
+        """Run language detection."""
         sc = SparkContext()
         data = sc.textFile(self.input().path)
         results = data.map(lambda x: self.detect_language(x)).collect()
@@ -246,8 +233,8 @@ class LanguageDetector(luigi.Task):
                 f.write(result + '\n')
         sc.stop()
 
-    def output(self):
-        """Override file at given path without footer."""
-        return luigi.contrib.hdfs.HdfsTarget('/pipeline/language_detected/' +
-                                             DATETIMESTAMP +
-                                             '_language_detected.txt')
+    def detect_language(self, input):
+        """Add language to each entry."""
+        document = json.loads(input)
+        document['lang'] = detect(document['full_body'])
+        return json.dumps(document, ensure_ascii=False)
