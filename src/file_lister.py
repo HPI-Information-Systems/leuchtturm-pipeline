23e22871ede07dba32e2ebb670d657f4b518c6f8
Jonas Hering <jonas.hering@student.hpi.de>
2018-01-10 10:07:56 +0100

    Add loop for hdfs files, ref meta#131

--- src/file_lister.py
+++ src/file_lister.py
@@ -1,13 +1,15 @@
 """This collects and dumps text documents to a spark rdd."""
 
 import json
+from hdfs import InsecureClient
 import findspark
 findspark.init('/usr/hdp/2.6.3.0-235/spark2')
 from pyspark import SparkContext
 
 
 input_folder = 'hdfs://172.18.20.109/enron_text'
-output_path = '/path/to/output/data-frame'
+output_path = 'hdfs://172.18.20.109/pipeline/files_listed'
+hdfs_client = InsecureClient('http://b7689.byod.hpi.de:50070')
 
 
 """Read all txt documents from a folder and collect them in one rdd.
@@ -27,10 +29,20 @@ def collect_files():
 
     sc = SparkContext()
 
-    rdd = sc.wholeTextFiles(input_folder, minPartitions=None, use_unicode=True)
+    rdd = sc.emptyRDD()
+
+    for directory in hdfs_client.list(input_folder):
+        for subdirectory in hdfs_client.list(input_folder + '/' + directory):
+            rdd = rdd.union(sc.wholeTextFiles(input_folder + '/' + directory + '/' + subdirectory,
+                                              minPartitions=None,
+                                              use_unicode=True))
+
+    print(rdd.count())
 
     rdd = rdd.filter(lambda x: filter_emails(x)) \
              .map(lambda x: create_document(x))
+
+    print(rdd.count())
 
     rdd.saveAsTextFile(output_path)
 
