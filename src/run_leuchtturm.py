bd197732850bd00a78b8bda4e7b9ad8b36d02930
Jonas Hering <jonas.hering@student.hpi.de>
2018-01-23 13:41:45 +0100

    Add working pipeline, ref meta#145

---
+++ src/run_leuchtturm.py
@@ -0,0 +1,41 @@
+"""This module runs pipeline tasks in correct order."""
+
+from leuchtturm import split_emails
+from leuchtturm import extract_metadata
+from leuchtturm import deduplicate_emails
+from leuchtturm import clean_bodies
+from leuchtturm import detect_languages
+from leuchtturm import extract_entities
+from pyspark import SparkContext
+
+
+input_path = 'hdfs://172.18.20.109/pipeline/files_listed_Jan_12'
+output_path = 'hdfs://172.18.20.109/pipeline/pipeline_results'
+
+
+def run_email_pipeline():
+    """Run entire text processing pipeline.
+
+    Requires: File listing.
+    Arguments: none.
+    Returns: void.
+    """
+    sc = SparkContext()
+
+    # set minPartitions to executors * cores per executor * 3
+    data = sc.textFile(input_path, minPartitions=54)
+
+    data = split_emails(data)
+    # data = extract_metadata(data)
+    # data = deduplicate_emails(data)
+    data = clean_bodies(data)
+    data = detect_languages(data)
+    data = extract_entities(data)
+
+    data.saveAsTextFile(output_path)
+
+    sc.stop()
+
+
+if __name__ == '__main__':
+    run_email_pipeline()
