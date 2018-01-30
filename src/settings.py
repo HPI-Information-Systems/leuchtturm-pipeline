ea33fb53b6a3064846ed3e7b5094cdbca5afcd9f
Jonas Hering <jonas.hering@student.hpi.de>
2018-01-30 15:50:41 +0100

    Fix re sub

--- src/settings.py
+++ src/settings.py
@@ -5,9 +5,9 @@ import re
 
 path_emails_raw = 'hdfs://172.18.20.109/LEUCHTTURM/enron_nuix_complete/*/*/*'
 path_files_listed = 'hdfs://172.18.20.109/LEUCHTTURM/tmp/files_listed'
-path_files_listed_short = re.sub(r'^hdfs://(\d{1,3}\.){3}\d{1,3}', path_files_listed, '')
+path_files_listed_short = re.sub(r'^hdfs://(\d{1,3}\.){3}\d{1,3}', '', path_files_listed)
 path_pipeline_results = 'hdfs://172.18.20.109/LEUCHTTURM/tmp/pipeline_results'
-path_pipeline_results_short = re.sub(r'^hdfs://(\d{1,3}\.){3}\d{1,3}', path_pipeline_results, '')
+path_pipeline_results_short = re.sub(r'^hdfs://(\d{1,3}\.){3}\d{1,3}', '', path_pipeline_results)
 solr_collection = 'enron_nuix_complete'
 
 hdfs_client_url = 'http://172.18.20.109:50070'
