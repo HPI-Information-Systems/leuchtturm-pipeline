d8635d13368d9db57563cfae8f8e38ed65e87963
Jonas Hering <jonas.hering@student.hpi.de>
2018-02-01 14:00:41 +0100

    Fix testset

--- src/settings.py
+++ src/settings.py
@@ -3,13 +3,13 @@
 import re
 
 
-path_emails_raw = 'hdfs://172.18.20.109/LEUCHTTURM/enron_calo_flat/*/*'
-path_files_listed = 'hdfs://172.18.20.109/LEUCHTTURM/tmp/files_listed_calo_daily'
+path_emails_raw = 'hdfs://172.18.20.109/LEUCHTTURM/enron_nuix_testset/*/*/*'
+path_files_listed = 'hdfs://172.18.20.109/LEUCHTTURM/tmp/files_nuix_daily'
 path_files_listed_short = re.sub(r'^hdfs://(\d{1,3}\.){3}\d{1,3}', '', path_files_listed)  # don't touch!
-path_pipeline_results = 'hdfs://172.18.20.109/LEUCHTTURM/tmp/pipeline_results_calo_daily'
+path_pipeline_results = 'hdfs://172.18.20.109/LEUCHTTURM/tmp/pipeline_nuix_daily'
 path_pipeline_results_short = re.sub(r'^hdfs://(\d{1,3}\.){3}\d{1,3}', '', path_pipeline_results)  # don't touch!
 
-solr_collection = 'enron_calo_daily'
+solr_collection = 'enron_nuix_daily'
 
 hdfs_client_url = 'http://172.18.20.109:50070'
 solr_client_url = 'http://b1184.byod.hpi.de:8983/solr' + '/' + solr_collection
