d4533ed92e66c4cf770a66c5038b2c797a10fd2c
Jonas Hering <jonas.hering@student.hpi.de>
2018-03-02 19:23:43 +0100

    Fix file lister cmd

--- deploy_pipeline.sh
+++ deploy_pipeline.sh
@@ -12,7 +12,7 @@ echo 'Deleting files_listed_dev on hdfs ...'
 hdfs dfs -rm -r tmp/files_listed_dev || true
 echo 'Running file lister'
 export SPARK_HOME=/usr/hdp/2.6.2.0-205/spark2/
-PYSPARK_PYTHON=./LEUCHTTURM_ENV/leuchtturm_env/bin/python spark-submit --master yarn --deploy-mode cluster --driver-memory 4g --executor-memory 4g --num-executors 6 --executor-cores 3 --archives leuchtturm_env.zip#LEUCHTTURM_ENV --py-files settings.py
+PYSPARK_PYTHON=./LEUCHTTURM_ENV/leuchtturm_env/bin/python spark-submit --master yarn --deploy-mode cluster --driver-memory 4g --executor-memory 4g --num-executors 6 --executor-cores 3 --archives leuchtturm_env.zip#LEUCHTTURM_ENV --py-files settings.py file_lister.py
 echo 'Deleting pipeline_results_dev on hdfs ...'
 hdfs dfs -rm -r tmp/pipeline_results_dev || true
 echo 'Running leuchtturm pipeline...'
