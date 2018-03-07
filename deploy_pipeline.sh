40a985d94fe4226de31ebc429bfb37968566b87c
Jonas Hering <jonas.hering@student.hpi.de>
2018-03-07 10:08:19 +0100

    Run db upload only with logging

--- deploy_pipeline.sh
+++ deploy_pipeline.sh
@@ -26,29 +26,29 @@ else
 fi
 
 echo '[stage 1 of 4] Building conda environment ...'
-conda create -n leuchtturm_env python=3.6 -y --copy || true
-source activate leuchtturm_env
-pip install --quiet -r requirements.txt
-cp -r ~/anaconda2/envs/leuchtturm_env . && cd leuchtturm_env && zip -r --quiet leuchtturm_env.zip * && mv leuchtturm_env.zip .. && cd .. || return
-cp ~/gitlab-runner/models/* models/ && cd models && zip --quiet models.zip * && mv models.zip .. && cd .. || return
-source deactivate
+# conda create -n leuchtturm_env python=3.6 -y --copy || true
+# source activate leuchtturm_env
+# pip install --quiet -r requirements.txt
+# cp -r ~/anaconda2/envs/leuchtturm_env . && cd leuchtturm_env && zip -r --quiet leuchtturm_env.zip * && mv leuchtturm_env.zip .. && cd .. || return
+# cp ~/gitlab-runner/models/* models/ && cd models && zip --quiet models.zip * && mv models.zip .. && cd .. || return
+# source deactivate
 
-echo '[stage 2 of 4] Running file lister ...'
-hdfs dfs -rm -r $FLISTER || true
-PYSPARK_PYTHON=./leuchtturm_env/bin/python \
-    spark-submit --master yarn --deploy-mode cluster \
-    --driver-memory 8g --executor-memory 4g --num-executors 23 --executor-cores 4 \
-    --archives leuchtturm_env.zip#leuchtturm_env \
-    --py-files src/settings.py \
-    src/file_lister.py $EMAILS $FLISTER
-echo '[stage 3 of 4] Running leuchtturm pipeline ...'
-hdfs dfs -rm -r $PRESULT || true
-PYSPARK_PYTHON=./leuchtturm_env/bin/python \
-    spark-submit --master yarn --deploy-mode cluster \
-    --driver-memory 8g --executor-memory 4g --num-executors 23 --executor-cores 4 \
-    --archives leuchtturm_env.zip#leuchtturm_env,models.zip#models \
-    --py-files src/settings.py,src/leuchtturm.py \
-    src/run_leuchtturm.py $FLISTER $PRESULT
+# echo '[stage 2 of 4] Running file lister ...'
+# hdfs dfs -rm -r $FLISTER || true
+# PYSPARK_PYTHON=./leuchtturm_env/bin/python \
+#     spark-submit --master yarn --deploy-mode cluster \
+#     --driver-memory 8g --executor-memory 4g --num-executors 23 --executor-cores 4 \
+#     --archives leuchtturm_env.zip#leuchtturm_env \
+#     --py-files src/settings.py \
+#     src/file_lister.py $EMAILS $FLISTER
+# echo '[stage 3 of 4] Running leuchtturm pipeline ...'
+# hdfs dfs -rm -r $PRESULT || true
+# PYSPARK_PYTHON=./leuchtturm_env/bin/python \
+#     spark-submit --master yarn --deploy-mode cluster \
+#     --driver-memory 8g --executor-memory 4g --num-executors 23 --executor-cores 4 \
+#     --archives leuchtturm_env.zip#leuchtturm_env,models.zip#models \
+#     --py-files src/settings.py,src/leuchtturm.py \
+#     src/run_leuchtturm.py $FLISTER $PRESULT
 
 echo '[stage 4 of 4] Running db uploads ...'
 source activate leuchtturm_env
