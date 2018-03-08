b9b521e177dc433c01bd56f149e00b75ddec8c56
Nico Scordialo <nico.scordialo@student.hpi.uni-potsdam.de>
2018-03-08 10:44:26 +0100

    Change models paths in settings. Ref meta#192.

--- src/settings.py
+++ src/settings.py
@@ -4,9 +4,8 @@
 PATH_EMAILS_RAW = 'enron_calo'
 PATH_FILES_LISTED = 'tmp/files_listed_dev'
 PATH_PIPELINE_RESULTS = 'tmp/pipeline_results_dev'
-PATH_LDA_MODEL = 'models/models/pickled_lda_model.p'
-PATH_LDA_DICT = 'models/models/pickled_lda_dictionary.p'
-PATH_LDA_TRAINING_DATA = 'models/training'
+PATH_LDA_MODEL = 'models/pickled_lda_model.p'
+PATH_LDA_DICT = 'models/pickled_lda_dictionary.p'
 
 SOLR_COLLECTION = 'enron_dev'
 
