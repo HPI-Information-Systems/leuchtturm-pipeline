14cda3601ee5fc641008f3d03fa5669f91cf91bf
Jonas Hering <jonas.hering@student.hpi.de>
2018-03-26 15:27:16 +0200

    Attempt to fix import error on spark submit

--- deploy_sopedu_pipeline.sh
+++ deploy_sopedu_pipeline.sh
@@ -23,8 +23,12 @@ echo '[stage 1 of 2] Building environment ...'
 conda create -n leuchtturm_env python=3.6 -y --copy || true
 source activate leuchtturm_env
 pip install --quiet -r requirements.txt
+# zip py environment
 cp -r ~/anaconda2/envs/leuchtturm_env . && cd leuchtturm_env && zip -r --quiet leuchtturm_env.zip * && mv leuchtturm_env.zip .. && cd .. || return
+#zip requirements for tm...
 cp ~/gitlab-runner/models/* models/ && cd models && zip --quiet models.zip * && mv models.zip .. && cd .. || return
+# zip src dir
+cd src && zip --quiet src.zip * && mv src.zip .. && cd .. || return
 source deactivate
 
 echo '[stage 2 of 2] Running leuchtturm pipeline. This might take a while ...'
@@ -33,8 +37,7 @@ curl $SOLR/update\?commit\=true -d  '<delete><query>*:*</query></delete>' || tru
 PYSPARK_PYTHON=./leuchtturm_env/bin/python \
     spark-submit --master yarn --deploy-mode cluster \
     --driver-memory 8g --executor-memory 4g --num-executors 23 --executor-cores 4 \
-    --archives leuchtturm_env.zip#leuchtturm_env,models.zip#models \
-    --py-files src/common.py,src/deduplication.py,src/ner.py,src/preprocessing.py,src/reader.py,src/topics.py,src/writer.py \
+    --archives leuchtturm_env.zip#leuchtturm_env,models.zip#models,src.zip#src \
     ./run_pipeline.py --read-from $EMAILS --write-to $PRESULT --solr --solr-url $SOLR 2>/dev/null
 
 echo -e '\n[Done]'
