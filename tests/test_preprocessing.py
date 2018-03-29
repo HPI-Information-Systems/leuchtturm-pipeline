79ba20a5c764fc1371388d574743e310da63a915
Jonas Hering <jonas.hering@student.hpi.de>
2018-03-29 16:51:27 +0200

    Add tests and mock data for preprocessig steps

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -1,57 +1,101 @@
 """Test preprocessing module."""
 
-import json
+import ujson as json
 
-from src.preprocessing import HeaderParsing, TextCleaning, LanguageDetection, EmailSplitting
-
-
-em = 'Message-ID: <26905545.1075861107045.JavaMail.evans@thyme>\r\nDate: Fri, 15 Feb 2002 09:07:17 -0800 (PST)\r\nFrom: susan.bailey@enron.com\r\nTo: sara.shackleton@enron.com\r\nSubject: RE: Thiele Kaolin Company\r\nMime-Version: 1.0\r\nContent-Type: text/plain; charset=us-ascii\r\nContent-Transfer-Encoding: 7bit\r\nX-From: Bailey, Susan </O=ENRON/OU=NA/CN=RECIPIENTS/CN=SBAILE2>\r\nX-To: Shackleton, Sara </O=ENRON/OU=NA/CN=RECIPIENTS/CN=Sshackl>\r\nX-cc: \r\nX-bcc: \r\nX-Folder: \\Susan_Bailey_Mar2002\\Bailey, Susan\\Deleted Items\r\nX-Origin: Bailey-S\r\nX-FileName: sbaile2 (Non-Privileged).pst\r\n\r\n\nSara,\n\nAs in our past exercises for the \"Non-Terminated/In-the-Money\" Counterparties, the Thiele-Kaolin Company continues to appear as a \"financial\" trading relationship.\n\nHowever, according to the Confirmation Desk there are only physical arrangements with this counterparty (as evidenced by our GTC docs).  I hope that Credit can resolve the mystery???\n\nSusan  \n -----Original Message-----\nFrom: \tShackleton, Sara  \nSent:\tFriday, February 15, 2002 10:21 AM\nTo:\tMcMichael Jr., Ed; Dicarlo, Louis; Moran, Tom; Apollo, Beth\nCc:\tBailey, Susan; Boyd, Samantha; Panus, Stephanie\nSubject:\tThiele Kaolin Company\n\nWe cannot locate any financial contracts for this counterparty.\n\nPlease verify that this party belongs on the ITM Non-terminated financial gas list.\n\nSara Shackleton\nEnron Wholesale Services\n1400 Smith Street, EB3801a\nHouston, TX  77002\nPh:  (713) 853-5620\nFax: (713) 646-3490'  # NOQA
-
-doc = json.dumps({'raw': em, 'doc_id': '1'})
-
-
-def test_header_parsing():
-    """Header fields are present."""
-    result = json.loads(HeaderParsing().run_on_document(doc))
-    assert 'header' in result
-    assert 'susan.bailey@enron.com' == result['header']['sender']['email']
-    assert 'RE: Thiele Kaolin Company' == result['header']['subject']
-    assert 'sara.shackleton@enron.com' == result['header']['recipients'][0]['email']
+from src.preprocessing import EmailDecoding, HeaderParsing, TextCleaning, LanguageDetection, EmailSplitting
+from .mock_preprocessing import decode_raw_simple, decode_raw_multipart  # decoding
+from .mock_preprocessing import splitting_raw_org_msg, splitting_raw_fwd, splitting_raw_dnc  # splitting
+from .mock_preprocessing import (header_raw_indent, header_parsed_indent, header_raw_fwd, header_parsed_fwd,
+                                 header_raw_regular, header_parsed_regular, header_raw_dnc, header_parsed_dnc,
+                                 header_raw_deformed, header_parsed_deformed)  # header parsing
+from .mock_preprocessing import lang_en_raw, lang_de_raw  # lang detection
+from .mock_preprocessing import clean_raw
 
 
-def test_header_body_separation():
-    """Main header is not in body part."""
-    result = json.loads(HeaderParsing().run_on_document(doc))
-    assert 'body' in result
-    assert 'Message-ID: <26905545.1075861107045.JavaMail.evans@thyme>' not in result['body']
-    assert 'As in our past exercises for the' in result['body']
+def test_email_decoding_simple():
+    """Simple emails should be decoded correctly."""
+    tool = EmailDecoding()
+    decoded = tool.run_on_document(decode_raw_simple)
+    assert len(decode_raw_simple) >= len(decoded)
+    assert 'Any questions, contact Debra Kimmel at 212-723-9472' in decoded
+
+
+def test_email_decoding_mime():
+    """Multipart emails should be decoded correctly."""
+    tool = EmailDecoding()
+    decoded = tool.run_on_document(decode_raw_multipart)
+    assert len(decode_raw_multipart) > len(decoded)
+    assert 'fkBgWKkHnKAH8cgSedunGBcGh4k6QNbXVCFh1+FLXQefWeg' not in decoded
+    assert 'I\'ll call him back but the boss won\'t call him' in decoded
+
+
+def test_email_splitting_on_org_msg():
+    """Emails should be splitted into their parts. Standard inline headers should be recognised."""
+    tool = EmailSplitting()
+    assert len(tool.run_on_document(splitting_raw_org_msg)) == 2
+
+
+def test_email_splitting_on_fwd():
+    """Emails should be splitted into their parts. Fwd headers should be recognised."""
+    tool = EmailSplitting()
+    assert len(tool.run_on_document(splitting_raw_fwd)) == 2
+
+
+def test_email_splitting_on_dnc():
+    """Emails should be splitted into their parts. Dnc headers should be recognised"""
+    tool = EmailSplitting()
+    assert len(tool.run_on_document(splitting_raw_dnc)) == 3
+
+
+def test_header_parsing_on_indented():
+    """Headers should be parsed correctly for visually indented headers (>>)."""
+    tool = HeaderParsing(clean_subject=False, use_unix_time=False)
+    assert tool.run_on_document(header_raw_indent) == header_parsed_indent
+
+
+def test_header_parsing_on_fwd():
+    """Headers should be parsed correctly for inline fwds."""
+    tool = HeaderParsing(clean_subject=False, use_unix_time=False)
+    assert tool.run_on_document(header_raw_fwd) == header_parsed_fwd
+
+
+def test_header_parsing_on_regular():
+    """Standard headers should be parsed correctly."""
+    tool = HeaderParsing(clean_subject=False, use_unix_time=False)
+    assert tool.run_on_document(header_raw_regular) == header_parsed_regular
+
+
+def test_header_parsing_on_dnc():
+    """Dnc headers should be parsed correctly."""
+    tool = HeaderParsing(clean_subject=False, use_unix_time=False)
+    assert tool.run_on_document(header_raw_dnc) == header_parsed_dnc
+
+
+def test_header_parsing_on_deformed():
+    """Deformed headers should be parsed correctly (e.g. no from field, instead startig with name)."""
+    tool = HeaderParsing(clean_subject=False, use_unix_time=False)
+    assert tool.run_on_document(header_raw_deformed) == header_parsed_deformed
+
+
+def test_language_detection():
+    """Language should be detected correctly on at least English and German texts."""
+    tool = LanguageDetection(read_from='body')
+    assert json.loads(tool.run_on_document(lang_en_raw))['lang'] == 'en'
+    assert json.loads(tool.run_on_document(lang_de_raw))['lang'] == 'de'
 
 
 def test_text_cleaning():
-    """Text cleaning doesn't leave unnormalized whitespace."""
-    text = json.dumps({'body': '\n\n\n\n\r\rThis is awesome!!!!\n\n\nGreat!'})
-    result = json.loads(TextCleaning().run_on_document(text))
-    assert 'text_clean' in result
-    assert 'This is awesome!!!!\nGreat!' == result['text_clean']
+    """Text cleaning should normalize whitespace and replace unicode chars."""
+    tool = TextCleaning(read_from='body', write_to='body')
+    cleaned = tool.run_on_document(clean_raw)
+    assert '🎨' not in cleaned
+    assert '\n\n' not in cleaned
 
 
-def test_language_detection_en():
-    """Test lang detect on english text."""
-    text = json.dumps({'body': 'I am a text written in a popular language!'})
-    result = json.loads(LanguageDetection(read_from='body').run_on_document(text))
-    assert 'lang' in result
-    assert 'en' == result['lang']
-
-
-def test_language_detection_de():
-    """Test lang detect on weird text."""
-    text = json.dumps({'body': 'Ich bin ein Text in einer beliebten Sprache!'})
-    result = json.loads(LanguageDetection(read_from='body').run_on_document(text))
-    assert 'lang' in result
-    assert 'de' == result['lang']
-
-
-def test_email_splitting():
-    """Email is plit into conversational parts."""
-    result = EmailSplitting().run_on_document(doc)
-    assert len(result) == 2
+def test_text_cleaning_strict():
+    """Text cleaning strict should additionally remove emails, urls, telephone numbers, ..."""
+    tool = TextCleaning(read_from='body', write_to='body', readable=False)
+    cleaned = tool.run_on_document(clean_raw)
+    assert '@enron.com' not in cleaned
+    assert 'https://' not in cleaned
+    assert '0049331' not in cleaned
