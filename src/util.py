16a6140c72658eb374c9f1503f5e631f28d09d7d
Jonas Hering <jonas.hering@student.hpi.de>
2018-04-24 16:55:00 +0200

    Remove abspath for spark

--- src/util.py
+++ src/util.py
@@ -1,4 +1,5 @@
 """Utils."""
+
 import configparser
 from pathlib import PurePath
 
@@ -6,7 +7,7 @@ from pathlib import PurePath
 def get_config(dataset):
     """Get a config and parse it."""
     config_file = 'config-' + dataset + '.ini'
-    configpath = PurePath('.') / 'config' / config_file
     config = configparser.ConfigParser()
-    config.read(str(configpath))
+    config.read('./config/' + config_file)
+
     return config
