7b24a424a7ba8c9af2a6edca8ac90b449386b364
Moritz Hartmann <moritz.hartmann@student.hpi.uni-potsdam.de>
2018-04-26 13:30:37 +0200

    Add sopedu connection

--- src/graph_analysis.py
+++ src/graph_analysis.py
@@ -3,9 +3,21 @@ import networkx as nx
 
 
 class GraphAnalyser:
+    def __init__(self,
+                 solr_url='http://sopedu.hpi.uni-potsdam.de:8983/solr/emails',
+                 neo4j_host='http://sopedu.hpi.uni-potsdam.de',
+                 http_port=60100,
+                 bolt_port=60000):
+        """Set solr config and path where rdd is read from."""
+        self.solr_url = solr_url
+        # querybuilder
+        self.neo4j_host = neo4j_host
+        self.http_port = http_port
+        self.bolt_port = bolt_port
 
     def analyse_graph(self):
-        neo_connection = py2neo.Graph()
+        print(self.neo4j_host)
+        neo_connection = py2neo.Graph(self.neo4j_host, http_port=self.http_port, bolt_port=self.bolt_port)
         edges = neo_connection.run('MATCH (source)-[r]->(target) RETURN id(source), id(target)')
 
         graph = nx.Graph()
@@ -13,8 +25,8 @@ class GraphAnalyser:
         for edge in edges:
             graph.add_edge(edge['id(source)'], edge['id(target)'])
 
-        graph = self.detect_communities(graph)
-        graph = self.detect_social_hierarchy(graph)
+        #graph = self.detect_communities(graph)
+        #graph = self.detect_social_hierarchy(graph)
         self.upload_graph(graph)
 
 
@@ -32,9 +44,11 @@ class GraphAnalyser:
         n = 0
         for clique in cliques:
             n += 1
-            print(n)
+        print(n)
 
     def upload_graph(self, graph):
         print(graph.number_of_nodes())
         print(graph.number_of_edges())
-    
+
+ga = GraphAnalyser()
+ga.analyse_graph()
