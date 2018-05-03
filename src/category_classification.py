5fa9ec17f91253b9aabea094f0628ffd30dd9faf
Jonas Hering <jonas.hering@student.hpi.de>
2018-05-04 01:29:32 +0200

    Add baseline (wip)

--- src/category_classification.py
+++ src/category_classification.py
@@ -1,9 +1,9 @@
 """Classification of emails in the pipeline."""
 
 import ujson as json
-from random import uniform
-from operator import itemgetter
+
 from .common import Pipe
+from .libs.clf import email_clf
 
 
 class EmailCategoryClassification(Pipe):
@@ -16,79 +16,17 @@ class EmailCategoryClassification(Pipe):
     def __init__(self):
         """Initialization."""
         super().__init__()
-        self.fake_categories = ['business', 'personal', 'spam']
-        self.fake_subcategories = ['scheduling', 'strategy', 'family', 'sports']
 
-    def load_model(self):
-        """Load model from path."""
-        raise NotImplementedError
-
-    def get_category_for_document(self, document, model=None):
-        """Predict classes for an email document, enable fake classes."""
-        def get_fake_categories():
-            prob_category = dict()
-            a = uniform(0, 1)
-            b = uniform(0, 1 - a)
-            c = 1 - a - b
-            for category, value in zip(self.fake_categories, [a, b, c]):
-                prob_category[category] = value
-
-            return prob_category
-
-        def get_fake_subcategories():
-            prob_subcategory = []
-            for subcategory in self.fake_subcategories:
-                prob_subcategory.append(
-                    {'name': subcategory, 'prob': uniform(0, 1)}
-                )
-
-            return prob_subcategory
-
-        def find_max(prob_category):
-            return max(prob_category.items(), key=itemgetter(1))[0]
-
-        def find_max_sub(prob_subcategory):
-            maximum = ''
-            current = 0
-            for dic in prob_subcategory:
-                if dic['prob'] > current:
-                    maximum = dic['name']
-                    current = dic['prob']
-
-            return maximum
-
-        category = dict()
-        if not model:
-            prob_category = get_fake_categories()
-            prob_subcategory = get_fake_subcategories()
-        else:
-            prob_category = {}  # TODO get categories with model
-            prob_subcategory = {}  # TODO get subcategories with model
-
-        category['top_category'] = find_max(prob_category)
-        category['prob_category'] = prob_category
-
-        category['top_subcategory'] = find_max_sub(prob_subcategory)
-        category['prob_subcategory'] = prob_subcategory
-
-        return category
-
-    def run_on_document(self, email_doc, model=None):
+    def run_on_document(self, email_doc):
         """Predict classes for a document."""
         document = json.loads(email_doc)
 
-        category = self.get_category_for_document(document, model)
-        document['category'] = category
+        prediction = {'top_category': email_clf.get_email_category(document['raw']),
+                      'prob_category': email_clf.get_category_prob(document['raw'])}
+        document['category'] = prediction
 
         return json.dumps(document)
 
-    def run_on_partition(self, partition):
-        """Run task in spark context. Partitionwise for performance reasosn."""
-        model = None  # self.load_model()
-
-        for doc in partition:
-            yield self.run_on_document(doc, model=model)
-
     def run(self, rdd):
         """Run task in spark context."""
-        return rdd.mapPartitions(lambda x: self.run_on_partition(x))
+        return rdd.map(lambda x: self.run_on_document(x))
---
+++ src/libs/clf/export.py
@@ -0,0 +1,66 @@
+"""Make classification steps available for pipeline."""
+
+from features import *
+from preprocessing import *
+import pandas as pd
+
+
+class EmailClfInterface(object):
+
+    def __init__(self, classifier, vectorizer_body, vectorizer_subject, label_encoder):
+        super().__init__()
+        self.classifier = classifier
+        self.vectorizer_body = vectorizer_body
+        self.vectorizer_subject = vectorizer_subject
+        self.label_encoder = label_encoder
+
+    def create_df(self, text):
+        return pd.DataFrame(data=[[text]], columns=['email'])
+
+    def preprocess(self, df):
+        return parse_raw_emails(df)
+
+    def add_features(self, df):
+        return add_all_features(df)
+
+    def transform(self, df):
+        return transform_features(df)
+
+    def vectorize_field(self, df, field, new_field, vectorizer):
+        return add_vectorized_field(df, field, new_field, vectorizer)
+
+    def drop_features(self, df):
+        return drop_features(df)
+
+    def prepare_email(self, text):
+        df = self.create_df(text)
+        df = self.preprocess(df)
+        df = self.add_features(df)
+        df = self.transform(df)
+        df = self.vectorize_field(df, 'body', 'b', self.vectorizer_body)
+        df = self.vectorize_field(df, 'subject', 's', self.vectorizer_subject)
+        df = self.drop_features(df)
+
+        return df
+
+    def classify_email(self, df):
+        return self.classifier.predict(df)[0]
+
+    def classify_email_proba(self, df):
+        return self.classifier.predict_proba(df)[0]
+
+    def translate_label(self, number):
+        return self.label_encoder.inverse_transform(number)
+
+    def predict_top_category(self, text):
+        df = self.prepare_email(text)
+        label = self.classify_email(df)
+
+        return self.translate_label(label)
+
+    def predict_proba(self, text):
+        df = self.prepare_email(text)
+        probabilities = self.classify_email_proba(df)
+        labels = self.label_encoder.classes_
+
+        return dict(zip(labels, probabilities))
