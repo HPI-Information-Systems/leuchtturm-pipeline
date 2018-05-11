97488d678a4440cf605f4066cac196f39a610ca7
Tim Repke <tim@repke.eu>
2018-05-11 12:56:10 +0200

    fix ini2bash variable handling

--- config/ini2bash.py
+++ config/ini2bash.py
@@ -21,5 +21,5 @@ from config import Config
 conf = Config()
 for section, options in conf.DEFAULTS.items():
     for option, value in options.items():
-        print(section.upper() + '_' + option.upper() + '="' + str(conf.get(section, option)).upper() + '"')
+        print(section.upper() + '_' + option.upper() + '="' + str(conf.get(section, option)) + '"')
 print('SOLR_URL="' + conf.solr_url + '"')
