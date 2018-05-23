06c4cf66455f154c5a6b7df754d4944aef5008a7
Moritz Hartmann <moritz.hartmann@student.hpi.uni-potsdam.de>
2018-05-23 14:42:20 +0200

    Update configs

--- config/config.py
+++ config/config.py
@@ -42,6 +42,7 @@ class Config:
         },
         'neo4j': {
             'import': False,
+            'upload': True,
             'protocol': 'http',
             'host': '0.0.0.0',
             'http_port': 7474,
